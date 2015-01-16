80d87150771452d7875bc51cf69d32237d8f4d09
tnclark8012 <tnclark8012@gmail.com>
2015-01-15 20:58:36 -0800

    Added DatesBetween

--- power-query-extensions.m
+++ power-query-extensions.m
@@ -2,6 +2,16 @@
 PQExtentions =
  [
  Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",
+ //Get a list of all dates between two end points
+ DatesBetween= (start as any, end as any) => 
+      let 
+        StartDate = Date.From(start), 
+        EndDate = Date.From(end),
+        adjustedStart = List.Min({StartDate, EndDate}),
+        adjustedEnd = List.Max({StartDate, EndDate}),
+        GetDates = (start as date, end as date, dates as list)=> if start > end then dates else @GetDates(Date.AddDays(start, 1), end, List.Combine({dates, {start}})),
+        Dates = GetDates(adjustedStart, adjustedEnd, {})
+      in Dates,
  // Is text all uppercase? returns false if any non-alpha characters are present
  IsUpperCase = (text as text) => List.AllTrue(List.Transform(Text.ToList(text), (letter)=>Text.Contains(Alphabet, letter) and letter = Text.Upper(letter))),
  // Splits camelCased and PascalCased text and separates by a space. Ex: "thisIsAColumn" -> "this Is A Column"
