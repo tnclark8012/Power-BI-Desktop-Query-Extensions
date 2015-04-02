f2ded611e47ebc3828f513afe8d6e76fbaa27181
Taylor Clark <tnclark8012@gmail.com>
2015-04-02 12:50:56 -0700

    Added prefixes and a few new functions
    ex: Text.RemoveExtraWhitespace

--- power-query-extensions.m
+++ power-query-extensions.m
@@ -1,31 +1,24 @@
 let 
-PQExtentions =
- [
- //
- // Text
- // 
- Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",
- // Is text all uppercase? returns false if any non-alpha characters are present
- IsUpperCase = (text as text) => List.AllTrue(List.Transform(Text.ToList(text), (letter)=>Text.Contains(Alphabet, letter) and letter = Text.Upper(letter))),
- // Splits camelCased and PascalCased text and separates by a space. Ex: "thisIsAColumn" -> "this Is A Column"
- 
- //
- // Table
- //
- SplitCamelCase = (text as nullable text) => if text is null then null else List.Accumulate(Text.ToList(text),"", (state, current) => let
-       PreviousLetter = Text.End(state, 1),
-       Ignore = (text as text) => text = " " or text = "."
-       in state & (if not IsUpperCase(PreviousLetter) and not Ignore(PreviousLetter) and not Ignore(current) and IsUpperCase(current) then " " else "" )& current),
- // Splits camelCased and PascalCased column names. 
- SplitColumnNames = (table as table) => Table.RenameColumns(table, List.Transform(Table.ColumnNames(table), each {_, SplitCamelCase(_)})), 
- // Splits camelCased and PascalCased text in a column. 
- SplitColumnText = (table as table, columns as list) => if List.Count(columns) = 0 then table else Table.TransformColumns(@SplitColumnText(table, List.Skip(columns, 1)), {{List.First(columns), SplitCamelCase}}),
- 
-//
-// Dates
-//
+Test = PQX[Text.RemoveExtraWhitespace]("Hello   there @ sdf"),
+PQX =[
+
+///////////////////////// 
+// Date                //
+/////////////////////////
+// Basic calendar
+Date.Calendar = (start as any, end as any) => 
+	let
+    		Source = pqx[Date.DatesBetween](StartDate, EndDate),
+    		FromList = Table.FromList(Source, Splitter.SplitByNothing(), null, null, ExtraValues.Error),
+    		Date = Table.RenameColumns(FromList,{{"Column1", "Date"}}),
+    		DayOfWeek = Table.AddColumn(Date, "Day of Week", each pqx[Date.DayName]([Date])),
+    		Month = Table.AddColumn(DayOfWeek, "Month", each pqx[Date.MonthName]([Date])),
+    		WeekStartDate = Table.AddColumn(Month, "WeekStartDate", each Date.StartOfWeek([Date])),
+    		WeekStart = Table.AddColumn(WeekStartDate, "Week Start", each [Month] & " " & Text.From(Date.Day([WeekStartDate])))
+	in
+    		WeekStart,
 // Dates between. Start and end can be flipped
-DatesBetween= (start as any, end as any) => 
+Date.DatesBetween= (start as any, end as any) => 
       let 
         StartDate = Date.From(start), 
         EndDate = Date.From(end),
@@ -34,14 +27,58 @@ DatesBetween= (start as any, end as any) =>
         GetDates = (start as date, end as date, dates as list)=> if start > end then dates else @GetDates(Date.AddDays(start, 1), end, List.Combine({dates, {start}})),
         Dates = GetDates(adjustedStart, adjustedEnd, {})
       in Dates,
+
 // Sunday is 0
-DayOfWeek = (dayOfWeekIndex as number) => Switch(dayOfWeekIndex, {0, 1, 2, 3, 4, 5, 6}, {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"}, null),
-MonthName  = (monthNum as number) => Switch(monthNum, {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}, 
+Date.DayName = (date as any) => Switch(Date.DayOfWeek(DateTime.From(date)), {0, 1, 2, 3, 4, 5, 6}, {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"}, null),
+
+Date.MonthName  = (date as any) => Switch(Date.Month(DateTime.From(date)), {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}, 
         {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"}, null),
  
- //
- // Utility
- //
- Switch = (value as any, cases as list, results as list, default as any) => if List.IsEmpty(cases) or List.IsEmpty(results) then default else if value = List.First(cases) then List.First(results) else @Switch(value, List.Skip(cases, 1), List.Skip(results, 1), default)    
- ]
-in PQExtentions
+
+
+///////////////////////// 
+// Text                //
+/////////////////////////
+Text.Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",
+
+// Is text all uppercase? returns false if any non-alpha characters are present
+Text.IsUpperCase = (text as text) => List.AllTrue(List.Transform(Text.ToList(text), (letter)=>Text.Contains(Text.Alphabet, letter) and letter = Text.Upper(letter))),
+
+Text.RemoveExtraWhitespace = (text as text) => Text.Combine(Splitter.SplitTextByWhitespace()(text)," "), 
+// Splits camelCased and PascalCased text and separates by a space. Ex: "thisIsAColumn" -> "this Is A Column"
+Text.SplitCamelCase = (text as nullable text) => if text is null then null else List.Accumulate(Text.ToList(text),"", (state, current) => 
+	let
+		PreviousLetter = Text.End(state, 1),
+		Ignore = (text as text) => text = " " or text = "."
+	in 
+		state & 
+		(if 
+			not Text.IsUpperCase(PreviousLetter) and 
+		 	not Ignore(PreviousLetter) and 
+		 	not Ignore(current) and 
+		 	Text.IsUpperCase(current) 
+		 then 
+			" " else "" ) & 
+		current),
+
+
+///////////////////////// 
+// Table               //
+/////////////////////////
+// Splits camelCased and PascalCased column names. 
+Table.SplitColumnNames = (table as table) => Table.RenameColumns(table, List.Transform(Table.ColumnNames(table), each {_, Text.SplitCamelCase(_)})), 
+
+// Splits camelCased and PascalCased text in a column. 
+ Table.SplitColumnText = (table as table, columns as list) => if List.Count(columns) = 0 then table else Table.TransformColumns(@Table.SplitColumnText(table, List.Skip(columns, 1)), {{List.First(columns), Text.SplitCamelCase}}),
+
+
+///////////////////////// 
+// Misc.               //
+/////////////////////////
+Switch = (value as any, cases as list, results as list, default as any) => if List.IsEmpty(cases) or List.IsEmpty(results) then default else if value = List.First(cases) then List.First(results) else @Switch(value, List.Skip(cases, 1), List.Skip(results, 1), default)    
+
+
+
+]
+in
+    PQX
