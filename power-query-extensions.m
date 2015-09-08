1bd520501aa22868c280e1dbaeefea590a92b0a4
Taylor Clark <tnclark8012@gmail.com>
2015-09-08 12:11:05 -0700

    Added MonthNumber, YearMonth to Date.Calendar

--- power-query-extensions.m
+++ power-query-extensions.m
@@ -1,4 +1,5 @@
-= [
+let
+    Source = [
 
 ///////////////////////// 
 // Date                //
@@ -13,10 +14,14 @@ Date.Calendar = (optional start as any, optional end as any) =>
          Date = Table.RenameColumns(FromList,{{"Column1", "Date"}}),
          DayOfWeek = Table.AddColumn(Date, "Day of Week", each Date.DayName([Date])),
          Month = Table.AddColumn(DayOfWeek, "Month", each Date.MonthName([Date])),
-         WeekStartDate = Table.AddColumn(Month, "WeekStartDate", each Date.StartOfWeek([Date])),
-         WeekStart = Table.AddColumn(WeekStartDate, "Week Start", each [Month] & " " & Text.From(Date.Day([WeekStartDate])))
+	 MonthNum = Table.AddColumn(Month, "MonthNumber", each Date.Month([Date])),
+         WeekStartDate = Table.AddColumn(MonthNum, "WeekStartDate", each Date.StartOfWeek([Date])),
+         WeekStart = Table.AddColumn(WeekStartDate, "Week Start", each [Month] & " " & Text.From(Date.Day([WeekStartDate]))),
+         Year = Table.AddColumn(WeekStart, "Year", each Date.Year([Date])),
+	 YearMonth = Table.AddColumn(Year, "YearMonth", each Number.From(Text.From([Year]) & (if [MonthNumber] < 10 then "0" else "") & Text.From([MonthNumber]))),
+         Result = YearMonth
    in
-         WeekStart,
+         Result,
 // Dates between. Start and end can be flipped
 Date.DatesBetween= (start as any, end as any) => 
       let 
@@ -206,3 +211,5 @@ Table.SplitColumnText = (table as table, columns as list) => if List.Count(colum
 Switch = (value as any, cases as list, results as list, optional default as any) => if List.IsEmpty(cases) or List.IsEmpty(results) then default else if value = List.First(cases) then List.First(results) else @Switch(value, List.Skip(cases, 1), List.Skip(results, 1), default)    
 
 ]
+in
+    Source
