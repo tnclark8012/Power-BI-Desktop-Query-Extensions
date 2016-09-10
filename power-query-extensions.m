4e312ded53aacde920f484ce3026a8301bc13c93
Taylor Clark <taylorc@microsoft.com>
2016-09-09 19:25:33 -0700

    reorder

--- power-query-extensions.m
+++ power-query-extensions.m
@@ -243,78 +243,44 @@ Splitter.SplitTextByNotIn = (safeCharacters as text) => (line as nullable text) 
         else  
           List.Combine({List.RemoveLastN(state, 1), {current}})),
 
-///////////////////////// 
-// Text                //
+
 /////////////////////////
-Text.Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",
-Text.AlphaNumeric = Text.Alphabet & "0123456789",
-Text.FromList = (list as list) => List.Accumulate(list, "", (state, current) => state & Text.From(current)),
-// Is text all uppercase? returns false if any non-alpha characters are present
-Text.IsUpperCase = (text as text) => List.AllTrue(List.Transform(Text.ToList(text), (letter)=>Text.Contains(Text.Alphabet, letter) and letter = Text.Upper(letter))),
-Text.IsAlpha = (text as text) => List.MatchesAll(Text.ToList(text), each Text.Contains(Text.Alphabet, _)),
-Text.RemoveExtraWhitespace = (text as text) => Text.Combine(Splitter.SplitTextByWhitespace()(text)," "), 
-// Splits camelCased and PascalCased text and separates by a space. Ex: "thisIsAColumn" -> "this Is A Column"
-Text.SplitCamelCase = (text as nullable text) => if text is null then null else List.Accumulate(Text.ToList(text),"", (state, current) => 
-   let
-      PreviousLetter = Text.End(state, 1),
-      Ignore = (text as text) => text = " " or text = "."
-   in 
-      state & 
-      (if 
-         not Text.IsUpperCase(PreviousLetter) and 
-         not Ignore(PreviousLetter) and 
-         not Ignore(current) and 
-         Text.IsUpperCase(current) 
-       then 
-         " " else "" ) & 
-      current),
+// Switch             //
+////////////////////////
 
-Text.SplitOnNotIn = (line as nullable text, validCharacters as text) => Splitter.SplitTextByNotIn(validCharacters)(line),
+Switch = 
+    Document(
+        "Switch", 
+        "Given a value, find it's paired item <br>"&
+            "Switch(value as any, cases as list, results as list, optional default as any) <br>" &
+            "Switch(value as any, pairs as list, optional default as any)",
+        { 
+            [ Description = "Using separate lists", Code = "Switch(1, {1, 2, 3}, {""A"", ""B"", ""C""})", Result = "A"],
+            [ Description = "Using one paired list", Code = "Switch(1, {{1, ""A""}, {2, ""B""}, {3, ""C""}})", Result = "A"]
+        },
+        (value as any, casesOrPairs as list, optional resultsOrDefault as any, optional default as any) =>
+            let
+               hasPairs = List.First(casesOrPairs) is list,
+               usingPairs = 
+                   let
+                       targetPosition = List.PositionOf(casesOrPairs, value, Occurrence.First, (case, theValue) => theValue = case{0})
+                   in
+                       if targetPosition = -1 then resultsOrDefault else casesOrPairs{targetPosition}{1},
+               usingCases = 
+                   let
+                       cases = casesOrPairs,
+                       results = resultsOrDefault
+                   in
+                       if List.IsEmpty(cases) or List.IsEmpty(results) then default else if value = List.First(cases) then List.First(results) else @Switch(value, List.Skip(cases, 1), List.Skip(results, 1), default)
+            in
+                if hasPairs then usingPairs else usingCases
+    ),
 
-Text.SplitOnNonAlpha = (line as nullable text) =>
-  if line is null then null else List.Accumulate(Text.ToList(line), {null} , (state, current) => 
-    let
-      doSkip = not Text.Contains(Text.Alphabet, current),
-      lastItem = List.Last(state),
-      appendLast = lastItem<>null
-    in
-      if doSkip then 
-        if lastItem is null then 
-          state 
-        else 
-          List.Combine({state, {null}})
-      else
-        if appendLast then
-          List.Combine({List.RemoveLastN(state, 1), {lastItem & current}})
-        else  
-          List.Combine({List.RemoveLastN(state, 1), {current}})),
-
-
-Text.Substring = (text as text, start as number, optional count as number) => 
-   let 
-      start = if start >= 0 then start else error "start index should be >= 0",
-      end = if count = null then Text.Length(text) else if count <= Text.Length(text) then count else error "count should be <= text length",
-      textList = Text.ToList(text),
-      substr = Text.FromList(List.FirstN(List.Skip(textList, start), end - start))
-   in substr,
-
-Text.IsNumber = (text as text) => try Number.FromText(text) is number otherwise false,
-Text.PositionAfter = (text as nullable text, substring as text) => 
-    let 
-        firstIndex = Text.PositionOf(text, substring),
-        indexAfter = if firstIndex >=0 then firstIndex + Text.Length(substring) else -1
-    in
-        if text is null then -1 else if indexAfter >= 0 and indexAfter < Text.Length(text) then indexAfter else -1,
-Text.Until = (text as text, endDelimiter as text, optional startIndex as number) => 
-    let
-        start = if startIndex = null then 0 else startIndex,
-        textFromStart = Text.Substring(text, start),
-        delimPosition = if Text.PositionOf(textFromStart, endDelimiter) >= 0 then Text.PositionOf(textFromStart, endDelimiter) else Text.Length(textFromStart)
-    in
-        if text is null then null else Text.Range(textFromStart, 0, delimPosition),
 ///////////////////////// 
 // Table               //
 /////////////////////////
+
+
 Table.AddBlankRow = (table as table) => Table.Combine({table, Table.FromRecords({[]})}),
 // Transforms a column's value into its nested value -- if it eventually finds only one. Consider the following column:
 //  MyCol
@@ -395,36 +361,83 @@ Table.SplitColumnText = (table as table, columns as list) => if List.Count(colum
 Table.TransformColumn = (table as table, column as text, transform as function) => Table.TransformColumns(table, {{column, transform}}),
 Table.RenameColumn = (table as table, column as text, newName as text) => Table.RenameColumns(table, {{column, newName}}),
 Table.RenameAndTransformColumn = (table, currentName as text, newName as text, transform as function) => Table.TransformColumn(Table.RenameColumns(table, {currentName, newName}), newName, transform),
+
 ///////////////////////// 
-// Misc.               //
+// Text                //
 /////////////////////////
-Switch = 
-    Document(
-        "Switch", 
-        "Given a value, find it's paired item <br>"&
-            "Switch(value as any, cases as list, results as list, optional default as any) <br>" &
-            "Switch(value as any, pairs as list, optional default as any)",
-        { 
-            [ Description = "Using separate lists", Code = "Switch(1, {1, 2, 3}, {""A"", ""B"", ""C""})", Result = "A"],
-            [ Description = "Using one paired list", Code = "Switch(1, {{1, ""A""}, {2, ""B""}, {3, ""C""}})", Result = "A"]
-        },
-        (value as any, casesOrPairs as list, optional resultsOrDefault as any, optional default as any) =>
-            let
-               hasPairs = List.First(casesOrPairs) is list,
-               usingPairs = 
-                   let
-                       targetPosition = List.PositionOf(casesOrPairs, value, Occurrence.First, (case, theValue) => theValue = case{0})
-                   in
-                       if targetPosition = -1 then resultsOrDefault else casesOrPairs{targetPosition}{1},
-               usingCases = 
-                   let
-                       cases = casesOrPairs,
-                       results = resultsOrDefault
-                   in
-                       if List.IsEmpty(cases) or List.IsEmpty(results) then default else if value = List.First(cases) then List.First(results) else @Switch(value, List.Skip(cases, 1), List.Skip(results, 1), default)
-            in
-                if hasPairs then usingPairs else usingCases
-    ),
+
+
+Text.Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",
+Text.AlphaNumeric = Text.Alphabet & "0123456789",
+Text.FromList = (list as list) => List.Accumulate(list, "", (state, current) => state & Text.From(current)),
+// Is text all uppercase? returns false if any non-alpha characters are present
+Text.IsUpperCase = (text as text) => List.AllTrue(List.Transform(Text.ToList(text), (letter)=>Text.Contains(Text.Alphabet, letter) and letter = Text.Upper(letter))),
+Text.IsAlpha = (text as text) => List.MatchesAll(Text.ToList(text), each Text.Contains(Text.Alphabet, _)),
+Text.RemoveExtraWhitespace = (text as text) => Text.Combine(Splitter.SplitTextByWhitespace()(text)," "), 
+// Splits camelCased and PascalCased text and separates by a space. Ex: "thisIsAColumn" -> "this Is A Column"
+Text.SplitCamelCase = (text as nullable text) => if text is null then null else List.Accumulate(Text.ToList(text),"", (state, current) => 
+   let
+      PreviousLetter = Text.End(state, 1),
+      Ignore = (text as text) => text = " " or text = "."
+   in 
+      state & 
+      (if 
+         not Text.IsUpperCase(PreviousLetter) and 
+         not Ignore(PreviousLetter) and 
+         not Ignore(current) and 
+         Text.IsUpperCase(current) 
+       then 
+         " " else "" ) & 
+      current),
+
+Text.SplitOnNotIn = (line as nullable text, validCharacters as text) => Splitter.SplitTextByNotIn(validCharacters)(line),
+
+Text.SplitOnNonAlpha = (line as nullable text) =>
+  if line is null then null else List.Accumulate(Text.ToList(line), {null} , (state, current) => 
+    let
+      doSkip = not Text.Contains(Text.Alphabet, current),
+      lastItem = List.Last(state),
+      appendLast = lastItem<>null
+    in
+      if doSkip then 
+        if lastItem is null then 
+          state 
+        else 
+          List.Combine({state, {null}})
+      else
+        if appendLast then
+          List.Combine({List.RemoveLastN(state, 1), {lastItem & current}})
+        else  
+          List.Combine({List.RemoveLastN(state, 1), {current}})),
+
+
+Text.Substring = (text as text, start as number, optional count as number) => 
+   let 
+      start = if start >= 0 then start else error "start index should be >= 0",
+      end = if count = null then Text.Length(text) else if count <= Text.Length(text) then count else error "count should be <= text length",
+      textList = Text.ToList(text),
+      substr = Text.FromList(List.FirstN(List.Skip(textList, start), end - start))
+   in substr,
+
+Text.IsNumber = (text as text) => try Number.FromText(text) is number otherwise false,
+Text.PositionAfter = (text as nullable text, substring as text) => 
+    let 
+        firstIndex = Text.PositionOf(text, substring),
+        indexAfter = if firstIndex >=0 then firstIndex + Text.Length(substring) else -1
+    in
+        if text is null then -1 else if indexAfter >= 0 and indexAfter < Text.Length(text) then indexAfter else -1,
+Text.Until = (text as text, endDelimiter as text, optional startIndex as number) => 
+    let
+        start = if startIndex = null then 0 else startIndex,
+        textFromStart = Text.Substring(text, start),
+        delimPosition = if Text.PositionOf(textFromStart, endDelimiter) >= 0 then Text.PositionOf(textFromStart, endDelimiter) else Text.Length(textFromStart)
+    in
+        if text is null then null else Text.Range(textFromStart, 0, delimPosition),
+
+
+///////////////////////// 
+// Value               //
+/////////////////////////
 
 Value.TypeText = (value as any) => 
     if value is binary then "binary" else
@@ -444,7 +457,7 @@ Value.TypeText = (value as any) =>
     if value is time then "time" else
     if value is type then "type" else
     if value is any then "any"
-    else Error "unknown -- not a primitive type!"
+    else error "unknown -- not a primitive type!"
 
 ],
 Result = _extensionLibrary
